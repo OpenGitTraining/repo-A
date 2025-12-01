54d716489f17fb609c253a744d36c541c6a0c05c
Pedro André Oliveira <pedro.andre.oliveira@icloud.com>
2025-12-01 21:16:12 +0900

    Adds tests

---
+++ tests/a.test.cpp
@@ -0,0 +1,13 @@
+#include "a.hpp"
+
+#include <print>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("get_a return the hex of a") {
+    constexpr auto expected = 'a' + 0;
+    CHECK(expected == get_a());
+}
